e4a1f4eb788b47f0e17487874c726c0a26555912
Philipp <26636274+PhilippFr@users.noreply.github.com>
2023-01-17 13:52:31 +0100

    remove unused and cleanup

--- cq_server_connector/cadq_server_connector.py
+++ cq_server_connector/cadq_server_connector.py
@@ -2,8 +2,6 @@ import json
 import sys
 
 import requests
-from cadquery import Assembly, Color
-from cadquery.cqgi import CQModel
 from jupyter_cadquery.base import _tessellate_group
 from jupyter_cadquery.cad_objects import to_assembly
 from jupyter_cadquery.utils import numpy_to_json
@@ -21,51 +19,6 @@ def get_json_model(assembly_children) -> list:
         raise CQServerConnectorError('An error occured when tesselating the assembly.') from error
 
     return json.loads(assembly_json)
-
-
-def get_result(cq_model: CQModel):
-    """Return a CQ assembly object composed of all models passed
-    to show_object and debug functions in the CadQuery script."""
-
-    result = cq_model.build()
-
-    if not result.success:
-        raise CQServerConnectorError('Error in model', result.exception)
-
-    return result
-
-
-def get_assembly(build_result):
-    MODEL_COLOR_DEFAULT = Color(0.9, 0.7, 0.1)
-    MODEL_COLOR_DEBUG = Color(1, 0, 0, 0.2)
-
-    assembly = Assembly()
-
-    for counter, result in enumerate(build_result.results):
-        rgb = result.options.get('color', None)
-        alpha = result.options.get('alpha', None)
-        name = result.options.get('name', None)
-
-        color = rgb if isinstance(rgb, Color) \
-            else Color(rgb) if isinstance(rgb, str) \
-            else Color(*rgb) if isinstance(rgb, tuple) \
-            else MODEL_COLOR_DEFAULT
-
-        if alpha:
-            color = Color(color.toTuple()[:3] + [alpha])
-
-        try:
-            assembly.add(result.shape, color=color, name=name)
-        except ValueError:
-            assembly.add(result.shape, color=color, name=f'{name}_{counter}')
-
-    for result in build_result.debugObjects:
-        assembly.add(result.shape, color=MODEL_COLOR_DEBUG)
-
-    if not assembly.children:
-        raise ValueError('nothing to show')
-
-    return assembly
 
 
 def get_data(module_name, json_model) -> dict:
@@ -91,8 +44,6 @@ class CQServerConnector:
         self.url = url
 
     def render(self, name, cq_model):
-        #result = get_result(cq_model)
-        #assembly = get_assembly(result)
         json_model = get_json_model(cq_model)
         json_data = get_data(name, json_model)
         self.post_data(json_data)
